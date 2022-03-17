c6b6b08bd4a30e581dd3e3fdb8e31ed233e7596a
Ida Lam <idaslam@outlook.com>
2022-03-17 15:20:31 +0100

    Grid: Grid component for the whole puzzle

---
+++ src/components/grid.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+import React from 'react'
+import { GridType } from '../helper/puzzle'
+import Row from './row'
+
+type GridProps = {
+  grid: GridType
+}
+
+const GridContainer = styled.div`
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    margin-top: 50px;
+`
+
+function Grid({ grid }: GridProps) {
+  return (
+    <GridContainer>
+      {grid && grid.map((row) => { return <Row key={row.toString()} row={row} /> })}
+    </GridContainer>
+  )
+}
+
+export default Grid
