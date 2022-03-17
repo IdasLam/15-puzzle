e394f06b00bc376c42108640cb5696269d28c8e7
Ida Lam <idaslam@outlook.com>
2022-03-17 15:44:02 +0100

    Puzzle: Component is aware of last tile

--- src/components/puzzle.tsx
+++ src/components/puzzle.tsx
@@ -1,10 +1,11 @@
-import styled from '@emotion/styled'
 import React from 'react'
-import { GridType } from '../helper/puzzle'
+import styled from '@emotion/styled'
+import { RowType } from '../helper/puzzle'
 import Row from './row'
 
 type GridProps = {
-  grid: GridType
+  grid: RowType[],
+  lastTile: number
 }
 
 const GridContainer = styled.div`
@@ -14,7 +15,8 @@ const GridContainer = styled.div`
     margin-top: 50px;
 `
 
-function Puzzle({ grid }: GridProps) {
+function Puzzle({ grid, lastTile }: GridProps) {
+  console.log(lastTile)
   return (
     <GridContainer>
       {grid && grid.map((row) => { return <Row key={row.toString()} row={row} /> })}
