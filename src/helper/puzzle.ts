c2b9f74b1695bfa202eeac515159c6522995abc3
Ida Lam <idaslam@outlook.com>
2022-03-18 11:02:02 +0100

    Puzzle: move tile and find index funtions

--- src/helper/puzzle.ts
+++ src/helper/puzzle.ts
@@ -3,8 +3,14 @@ type ParamsGrid = {
   columns: number
 }
 
+type MoveTile = { grid: GridType, tile: number, lastTile: number }
+
 export type RowType = number[]
 export type GridType = RowType[]
+export type GridIndexes = {
+  column: number,
+  row: number
+}
 
 export const shuffleTiles = ({ rows, columns }: ParamsGrid) => {
   const totalTiles = rows * columns
@@ -40,4 +46,31 @@ export const generateGrid = ({ rows, columns }: ParamsGrid): GridType => {
   }
 
   return grid
+}
+
+export const findIndex = ({ grid, tile }: { grid: GridType, tile: number }): GridIndexes | void => {
+  let foundColIndex = -1
+  let foundRowIndex = -1
+
+  grid.forEach((row, rowIndex) => {
+    row.forEach((gridTile, tileIndex) => {
+      if (tile === gridTile) {
+        foundColIndex = tileIndex
+        foundRowIndex = rowIndex
+      }
+    })
+  });
+
+  if (foundColIndex === -1 && foundRowIndex === -1) {
+    throw new Error('Index was not found')
+  }
+
+  return { row: foundRowIndex, column: foundColIndex }
+}
+
+export const moveTile = ({ grid, tile, lastTile } : MoveTile) => {
+  const indexOfTile = findIndex({ grid, tile })
+  const indexOfEmptyTile = findIndex({ grid, tile: lastTile })
+
+  console.log({ indexOfTile, indexOfEmptyTile })
 }
