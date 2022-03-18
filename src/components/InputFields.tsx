9ffdb97ee2ea1a492f1da9822207a3f3ca01a86e
Ida Lam <idaslam@outlook.com>
2022-03-18 17:27:08 +0100

    Inputfield: Lets user input rows and columns

---
+++ src/components/InputFields.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { TextField } from '@mui/material'
+import { GridCount } from '../helper/puzzle'
+
+const Input = styled.div`
+    display: flex;
+    gap: 1vw;
+    place-content: center;
+`
+
+type InputFieldsProps = {
+  colRow: GridCount
+  setColumnsRows: React.Dispatch<React.SetStateAction<{
+    columns: number;
+    rows: number;
+  }>>
+
+}
+
+function InputFields({ colRow, setColumnsRows }: InputFieldsProps) {
+  const { columns, rows } = colRow
+
+  const onChangeHandle = (value: string, type: 'rows' | 'columns') => {
+    const newValue = value === '' ? 0 : parseInt(value)
+    setColumnsRows({ ...colRow, [type]: newValue })
+  }
+
+  return (
+    <Input>
+      <TextField id="outlined-basic" label="Columns" variant="outlined" value={columns} size="small" onChange={(event) => { return onChangeHandle(event.target.value, 'columns') }} />
+      <TextField id="outlined-basic" label="Rows" variant="outlined" value={rows} size="small" onChange={(event) => { return onChangeHandle(event.target.value, 'rows') }} />
+    </Input>
+  )
+}
+
+export default InputFields
