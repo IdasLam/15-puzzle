91179a3874d65f7e40c80eab3135bc959b10e564
Ida Lam <idaslam@outlook.com>
2022-03-18 17:11:54 +0100

    App: Finnished will be prompted if the puzzle is solved. Generate answer and grid on mount.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from '@emotion/styled'
 import './App.css'
-import Puzzle from './components/puzzle'
-import { generateGrid } from './helper/puzzle'
+import { generateGrid, GridType } from './helper/puzzle'
 import 'normalize.css'
+import Puzzle from './components/puzzle'
 import LastTileContext from './contexts/lastTile'
+
+const Finnish = styled.div`
+
+`
 
 function App() {
   const [moves, setMoves] = useState(0)
-  // const [reShuffle, setReShuffle] = useState(false)
   const [{ columns, rows }] = useState({ columns: 4, rows: 4 })
-  const [grid, setGrid] = useState(generateGrid({ rows, columns }))
+  const [grid, setGrid] = useState<GridType>([])
   const [lastTile] = useState(columns * rows)
+  const [puzzleAnswer, setPuzzleAnswer] = useState<number[]>([])
+  const [gameFinnished, setGameFinnished] = useState(false)
+
+  useEffect(() => {
+    const { grid: newGrid, answer } = generateGrid({ rows, columns })
+
+    setGrid(newGrid)
+    setPuzzleAnswer(answer)
+  }, [])
+
+  useEffect(() => {
+    if (grid.length > 1 && JSON.stringify(grid) === JSON.stringify(puzzleAnswer)) {
+      setGameFinnished(true)
+    }
+  }, [grid])
 
   const addMove = () => {
     setMoves(moves + 1)
@@ -19,11 +38,17 @@ function App() {
   // options for col and rows
   return (
     <div className="App">
+      <h1>15 - Puzzle</h1>
       <p>
-        moves
+        Current moves :
         {' '}
-        {moves}
+        { moves }
       </p>
+      {gameFinnished && (
+      <Finnish>
+        <p>game finito</p>
+      </Finnish>
+      )}
       <LastTileContext.Provider value={lastTile}>
         <Puzzle grid={grid} addMove={addMove} setGrid={setGrid} />
       </LastTileContext.Provider>
